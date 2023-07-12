bfa05c75ccc6bea186d5ce00ce376db2b659fdb2
jamad <jamad2001@gmail.com>
2023-07-12 15:51:41 +0300

    Update 01_move_character.py

--- 01_move_character.py
+++ 01_move_character.py
@@ -12,7 +12,11 @@ class App:
         pyxel.run(self.update, self.draw) # アプリケーションの実行
 
     def update(self): # フレームの更新処理
-        self.x = (self.x + 1) % pyxel.width
+
+        # 右キーを押している間はxが増える
+        self.x = self.x + 1 if(pyxel.btn(pyxel.KEY_RIGHT)) else 1
+            
+        #self.x = (self.x + 1) % pyxel.width
 
     def draw(self): # 描画処理
         pyxel.cls(0)
