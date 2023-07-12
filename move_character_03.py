ec5d3f8cb9f02d5a993541f161620e84d66c9382
jamad <jamad2001@gmail.com>
2023-07-12 17:10:37 +0300

    Update move_character_03.py

--- move_character_03.py
+++ move_character_03.py
@@ -1,18 +1,14 @@
-# -*- coding: utf-8 -*-
-
 import pyxel
 
 class App:
-    def __init__(self): # 初期化
-        pyxel.init(160, 120)
+    def __init__(self): 
+        pyxel.init(160, 120, title="move rect by arrow keys")
         self.x = self.y = 0
         pyxel.run(self.update, self.draw) 
 
     def update(self): # calc data
-        self.x += (pyxel.btn(pyxel.KEY_RIGHT))
-        self.x -= (pyxel.btn(pyxel.KEY_LEFT))
-        self.y += (pyxel.btn(pyxel.KEY_DOWN))
-        self.y -= (pyxel.btn(pyxel.KEY_UP))
+        self.x += (pyxel.btn(pyxel.KEY_RIGHT))    - (pyxel.btn(pyxel.KEY_LEFT))
+        self.y += (pyxel.btn(pyxel.KEY_DOWN))     - (pyxel.btn(pyxel.KEY_UP))
       
     def draw(self): # draw data
         pyxel.cls(0)
