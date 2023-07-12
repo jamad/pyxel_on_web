6889fa0bcee40f6af3a7f2a6747ba9a44a6b6260
jamad <jamad2001@gmail.com>
2023-07-12 14:29:05 +0300

    Update 01_move_character.py

--- 01_move_character.py
+++ 01_move_character.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
+# assets/cat_16x16.png
+# jump_game.pyxres
+
 import pyxel
 
 class App:
     def __init__(self): # 初期化
         pyxel.init(160, 120)
+        pyxel.load("jump_game.pyxres")
         self.x = 0
         pyxel.run(self.update, self.draw) # アプリケーションの実行
 
