7f41644ccfe9cdf2469fc22b8a004bed2aa71415
jamad <jamad2001@gmail.com>
2023-07-12 15:47:18 +0300

    Update 01_move_character.py

--- 01_move_character.py
+++ 01_move_character.py
@@ -8,7 +8,6 @@ import pyxel
 class App:
     def __init__(self): # 初期化
         pyxel.init(160, 120)
-        pyxel.load("jump_game.pyxres")
         self.x = 0
         pyxel.run(self.update, self.draw) # アプリケーションの実行
 
@@ -18,5 +17,7 @@ class App:
     def draw(self): # 描画処理
         pyxel.cls(0)
         pyxel.rect(self.x, 0, 8, 8, 9)
+
+
 
 App()
