46cf6ed30826f75710e8883268745089dda6adaf
jamad <jamad2001@gmail.com>
2023-07-16 00:14:36 +0300

    Update art_by_code.py

--- art_by_code.py
+++ art_by_code.py
@@ -22,23 +22,9 @@ print(art.split())
 
 class App:
     def __init__(self):
-        pyxel.init(96, 54, title="Image API", display_scale=2)
-        self.niko = Niko()
-        pyxel.run(self.update, self.draw)
-
-    def update(self):
-        self.niko.update()
-
-    def draw(self):
-        pyxel.cls(0)
-        self.niko.draw()
-        pyxel.text(0, 0, 'Please SPACE key: ' + str(self.niko.color), 7)
-
-class Niko:
-    def __init__(self):
-        self.__set_image0()
-    
-    def __set_image0(self):
+        pyxel.init(96, 54, title="Image by code", display_scale=2)
+        #self.niko = Niko()
+        
         self.x = (96 / 2) - (16 / 2)
         self.y = (54 / 2) - (16 / 2)
         self.img = 0
@@ -47,17 +33,20 @@ class Niko:
         self.color = 7
         pyxel.image(0).set(0, 0, art.split())
 
+        pyxel.run(self.update, self.draw)
+
     def update(self):
         if pyxel.btnp(pyxel.KEY_SPACE):
-            self.__change_color()
+            self.color += 1
+            if 15 <= self.color: self.color = 1
 
     def draw(self):
+        pyxel.cls(0)
+        
         pyxel.pal(7, self.color)
         pyxel.blt(self.x, self.y, self.img, 0, 0, self.w, self.h, 0)
         pyxel.pal()
 
-    def __change_color(self):
-        self.color += 1
-        if 15 <= self.color: self.color = 1
+        pyxel.text(0, 0, 'col change by SPACE: ' + str(self.color), 7)
 
 App()
