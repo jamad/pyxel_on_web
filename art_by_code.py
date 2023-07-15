989c798ea4832b898e38893399e6c24395974cbf
jamad <jamad2001@gmail.com>
2023-07-16 00:41:08 +0300

    Update art_by_code.py

--- art_by_code.py
+++ art_by_code.py
@@ -1,24 +1,26 @@
 import pyxel
 
-art='''\
-0000077777700000
-0007700000077000
-0070000000000700
-0700000000000070
-0700000000000070
-7000070000700007
-7000070000700007
-7000000000000007
-7000000000000007
-7000000000000007
-7000700000070007
-0700070000700070
-0700007777000070
-0070000000000700
-0007700000077000
-0000077777700000
+my_img='''\
+.....######.....
+...##......##...
+..#..........#..
+.#............#.
+.#........#...#.
+#....#...#.....#
+#....#....#....#
+#..............#
+#......##......#
+#......##......#
+#...#......#...#
+.#...#....#.#.#.
+.#....####....#.
+..#..........#..
+...##......##...
+.....######.....
 '''
-print(art.split())
+
+my_img=[''.join('07'[c=='#'] for c in line) for line in my_img.split() ]
+#print(my_img)
 
 SCR_W=96
 SCR_H=54
@@ -29,7 +31,7 @@ class App:
         pyxel.init(SCR_W, SCR_H, title="Image by code", display_scale=2)
         self.color = 1
         self.img_id = 0
-        pyxel.image(self.img_id).set(0, 0, art.split())
+        pyxel.image(self.img_id).set(0, 0, my_img)
 
         pyxel.run(self.update, self.draw)
 
@@ -40,10 +42,10 @@ class App:
     def draw(self):
         pyxel.cls(0)
         
-        pyxel.pal(7, self.color)
-        pyxel.blt((SCR_W - W) // 2, (SCR_H - H) // 2, self.img_id, 0, 0, W, H, 0)
-        pyxel.pal()
-
+        pyxel.pal() # reset palette
         pyxel.text(0, 0, 'col change by SPACE: ' + str(self.color), 7)
+        
+        pyxel.pal(7, self.color) # override color 7 
+        pyxel.blt((SCR_W - W) // 2, (SCR_H - H) // 2, self.img_id, 0, 0, W, H, 0)
 
 App()
