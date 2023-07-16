372f829077b17f1a276a0ec189f30eef48ea80a6
jamad <jamad2001@gmail.com>
2023-07-16 13:02:09 +0300

    Update display_mandelbrot.py

--- display_mandelbrot.py
+++ display_mandelbrot.py
@@ -13,35 +13,43 @@ center_x=width//2
 center_y=height//2
 scaling=max(width,height)//3
 
-
-def mandelbrot(c, z=0, iteration_level=30): # true if dot stay 
-    for i in range(iteration_level):
-        z = z * z + c
-        if abs(z) > 2:
-            return False
-    return True
-
 class App:
-    
-    def __init__(self): 
+    def __init__(self):
         pyxel.init(width, height, title="mandelbrot", display_scale=my_disp_scale, fps=my_fps)
-        self.data={}
-        pyxel.run(self.update, self.draw) 
+        
+        #custom palette 
+        pyxel.load('custom_palette_gradation.pyxres')
 
-    def update(self):   
+        self.data = {}
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
         if not self.data:
             for r in range(height):
                 for c in range(width):
-                    a = (c - center_x) /scaling
-                    b = (r - center_y) /scaling
-                    self.data[(r,c)]=mandelbrot(complex(a, b))
+                    a = (c - center_x) / scaling
+                    b = (r - center_y) / scaling
+                    iteration = self.calculate_iteration(complex(a, b))
+                    self.data[(r, c)] = iteration
 
-    def draw(self): 
-        pyxel.cls(0)# clear screen
-    
+    def draw(self):
+        pyxel.cls(0) 
+
         for r in range(height):
             for c in range(width):
-                dotcolor=7*self.data[(r,c)] # black or white color
-                pyxel.pset(c, r, dotcolor)
+                iteration = self.data[(r, c)]
+                color = self.get_color(iteration)
+                pyxel.pset(c, r, color)
+
+    def calculate_iteration(self, c, z=0, iteration_level=16):
+        for i in range(iteration_level):
+            z = z * z + c
+            if abs(z) > 2:
+                return i
+        return iteration_level
+
+    def get_color(self, iteration):
+        color = iteration * 7 // 16  # color mapping
+        return color
 
 App()
