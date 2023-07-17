dbdbb2215fb65e49e10c09317e03443a66d721d2
jamad <jamad2001@gmail.com>
2023-07-17 21:44:22 +0300

    Update custom_font_display2.py

--- custom_font_display2.py
+++ custom_font_display2.py
@@ -6,45 +6,34 @@ import string
 import os
 import numpy as np
 
-class Font:
-    def __init__(self, file, size, str_to_disp):
-        self.file = file
-        self.size = size
-        self.alphabet = str_to_disp
 
-        px_w, px_h = size
-        font = ImageFont.truetype(file, size=px_h)
-        img = Image.new('1', size=(256, 256))
-        draw = ImageDraw.Draw(img)
-        coords = {}
-        x, y = 0, 0
-        for c in str_to_disp:
-            if x + px_w > 256:
-                x = 0
-                y += px_h
-            draw.text((x, y), c, font=font, fill=1)
-            coords[c] = (x, y)
-            x += px_w
-        self.coords = coords
-        self.img = img
-        self.data = np.array(img.getdata()).reshape(256, 256)
-
-
-# relative path based on this .py file
-fontfile = os.path.join(os.path.dirname(__file__), 'x8y12pxTheStrongGamer.ttf')
-
-char_size = (8, 12)
+fontfile_path = os.path.join(os.path.dirname(__file__), 'x8y12pxTheStrongGamer.ttf') # relative path based on this .py file
 
 ascii_chars = string.punctuation + string.digits + string.ascii_letters
 hiragana = "".join(chr(c) for c in range(ord('ぁ'), ord('ゔ') + 1)) + "ー"
 katakana = "".join(chr(c) for c in range(ord('ァ'), ord('ヶ') + 1)) + "ー"
-string_to_disp = f'font: {fontfile} {"-"*32}{ascii_chars + hiragana + katakana + "、。「」"}'
+string_to_disp = f'font: {fontfile_path} {"-"*32}{ascii_chars + hiragana + katakana + "、。「」"}'
 
-font = Font(fontfile, char_size, string_to_disp )
+screen_w=256
+screen_h=128+8
 
 class App:
     def __init__(self):
-        pyxel.init(256, 128+8, title="Custom Font Display", display_scale=1)
+        pyxel.init(screen_w, screen_h, title="Custom Font Display", display_scale=1)
+        
+        # font data 
+        px_w, px_h = (8, 12)
+        img = Image.new('1', size=(256, 256))
+        x, y = 0, 0
+        for c in string_to_disp:
+            if x + px_w > 256:
+                #print(x)
+                x = 0
+                y += px_h
+            ImageDraw.Draw(img).text((x, y), c, font=ImageFont.truetype(fontfile_path, size=px_h), fill=1)
+            x += px_w
+        self.font_data = np.array(img.getdata()).reshape(256, 256)
+
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -58,7 +47,7 @@ class App:
         img_bank = pyxel.image(img)
         for y in range(256):
             for x in range(256):
-                if font.data[y][x] != 0:
+                if self.font_data[y][x] != 0:
                     pyxel.pset(x, y, color) 
 
 App()
