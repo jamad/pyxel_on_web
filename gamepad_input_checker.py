38134ddc522bad9df186389de189135812eca385
jamad <jamad2001@gmail.com>
2023-07-29 12:15:41 +0300

    Update gamepad_input_checker.py

--- gamepad_input_checker.py
+++ gamepad_input_checker.py
@@ -1,8 +1,33 @@
 import pyxel
 
+my_img='''\
+0000000000000000
+00a0000000000a00
+00a0000000000a00
+0000000000000000
+0333300000033330
+0000000000000000
+0333000000003330
+3000300000030003
+3070309009030703
+3000300000030003
+0333000000003330
+0000000000800000
+0000200000000000
+0002220080008000
+0000200000000000
+0000000000800000
+'''.split() 
+
+my_img=[''.join(str(int('0'<c)) for c in line) for line in my_img ]
+        
+#print(my_img)
+
 class App:
     def __init__(self):
         pyxel.init(256, 256, title='gamepad input', display_scale=2)
+        self.img_id=0
+        pyxel.image(self.img_id).set(0, 0, my_img) # assigning string list to image(0)
 
         pyxel.run(self.update, self.draw)
 
@@ -25,5 +50,9 @@ class App:
             data_to_check=gamepadnum + 'BUTTON_' + x
             value=pyxel.btn(eval("pyxel."+ data_to_check))
             pyxel.text(10,80+10*i, f'{data_to_check} : {value}', value and 7 or 2)
+
+            
+        pyxel.blt(200, 10, self.img_id, 0, 0, 16, 16, 0)
+        pyxel.text(200,40, f'wip',  1)
 
 App()
