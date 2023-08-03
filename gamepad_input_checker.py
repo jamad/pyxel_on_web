98e70776874942202ddbbb318f3675c8e86605ef
jamad <jamad2001@gmail.com>
2023-08-04 01:31:14 +0300

    Update gamepad_input_checker.py

--- gamepad_input_checker.py
+++ gamepad_input_checker.py
@@ -37,16 +37,10 @@ myDict={} # name : value
 
 class App:
     def __init__(self):
-        pyxel.init(256, 512, title='gamepad input', display_scale=1)
+        pyxel.init(256, 256, title='gamepad input', display_scale=2)
 
         pyxel.image(image_base).set(0, 0, my_img) # assigning string list to image(0)
-        self.stick_LX=0
-        self.stick_LY=0
-        self.stick_RX=0
-        self.stick_RY=0
-        self.stick_LT=0
-        self.stick_RT=0
-        
+
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -63,16 +57,7 @@ class App:
             value=pyxel.btnv(eval("pyxel."+ data_to_check))
 
             keylist[i]=data_to_check
-            myDict[data_to_check]= ' '*(0<=value)+f'{value:016b}'#formatted_string = f'{value:+016b}' if value >= 0 else f'{(1 << 16) + value:016b}'
-            #myDict[data_to_check]= f'{value:+016b}' if value >= 0 else f'{(1 << 16) + value:016b}'
-            myDict[data_to_check]= f'{value}'
-
-            if x=='LEFTX':  self.stick_LX=value//4096
-            if x=='LEFTY':  self.stick_LY=value//4096
-            if x=='RIGHTX': self.stick_RX=value//4096
-            if x=='RIGHTY': self.stick_RY=value//4096
-            if x=='TRIGGERLEFT':    self.stick_LT=value//4096
-            if x=='TRIGGERRIGHT':   self.stick_RT=value//4096
+            myDict[data_to_check]=value
             
         button='A B X Y BACK GUIDE START LEFTSTICK RIGHTSTICK LEFTSHOULDER RIGHTSHOULDER DPAD_UP DPAD_DOWN DPAD_LEFT DPAD_RIGHT'.split() # e f g h i z j k l m n o p q r
         for i,x in enumerate(button):
@@ -90,93 +75,15 @@ class App:
 
         pyxel.image(image_triggered).set(0, 0, [''.join(R) for R in self.my_img_trigger]) # assigning data for highlight to image(1)
 
-        #print(myDict)
-
     def draw(self):
         pyxel.cls(0)
 
         # key state in text
         for i,data_to_check in keylist.items(): 
-            pyxel.text(10,10+10*i+256, f'{data_to_check} : {myDict[data_to_check]}', myDict[data_to_check] and 7 or 2)
+            pyxel.text(10,10+10*i, f'{data_to_check} : {myDict[data_to_check]}', myDict[data_to_check] and 7 or 2)
 
-        pyxel.blt(5, 5, image_base, 0, 0, 16, 16, 0)         # gamepad base image
-        pyxel.blt(5, 5, image_triggered, 0 ,0, 16, 16, 0)    # gamepad trigger image
-
-        ##### analog stick
-
-        #pyxel.line(100+self.stick_L_X,100+self.stick_L_Y,135+self.stick_R_X,100+self.stick_R_Y,8)
-        offsetX=20
-        offsetY=80
-        gapH=24+2
-        gap=16
-
-        color= myDict['GAMEPAD1_BUTTON_LEFTSTICK'] and 7 or 5
-        pyxel.circb(offsetX,offsetY,8,color)
-
-        color= myDict['GAMEPAD1_BUTTON_RIGHTSTICK'] and 7 or 5
-        pyxel.circb(offsetX+gapH,offsetY,8,color)
-
-        pyxel.line(offsetX,offsetY,offsetX+self.stick_LX,offsetY+self.stick_LY,7)
-        pyxel.pset(offsetX+self.stick_LX,offsetY+self.stick_LY,5) # green tip
-
-        pyxel.line(offsetX+gapH,offsetY,offsetX+gapH+self.stick_RX,offsetY+self.stick_RY,7)
-        pyxel.pset(offsetX+gapH+self.stick_RX,offsetY+self.stick_RY,5)# green tip
-
-        # triggers
-        pyxel.line(offsetX,offsetY-gap,offsetX,offsetY-gap-self.stick_LT,7)
-        pyxel.pset(offsetX,offsetY-gap-self.stick_LT,5) # green tip
-
-        pyxel.line(offsetX+gapH,offsetY-gap,offsetX+gapH,offsetY-gap-self.stick_RT,7)
-        pyxel.pset(offsetX+gapH,offsetY-gap-self.stick_RT,5) # green tip
-
-        # shoulder button
-        color= myDict['GAMEPAD1_BUTTON_LEFTSHOULDER'] and 7 or 5
-        pyxel.line(offsetX-6,offsetY-12,offsetX+6,offsetY-12,color)
-
-        color= myDict['GAMEPAD1_BUTTON_RIGHTSHOULDER'] and 7 or 5
-        pyxel.line(offsetX-6 +gapH ,offsetY-12,offsetX+6 +gapH,offsetY-12,color)
-
-        # start button
-        color= myDict['GAMEPAD1_BUTTON_BACK'] and 7 or 5
-        pyxel.rect(offsetX + 9 , offsetY +10, 2,2, color)
-
-        # start button
-        color= myDict['GAMEPAD1_BUTTON_START'] and 7 or 5
-        pyxel.rect(offsetX + gapH -9  , offsetY +10, 2,2, color)
-
-        # dpad
-        dpad_size=3
-        offsetX_dpad=offsetX-dpad_size//2
-        offsetY_dpad=offsetY +18
-        pyxel.rect(offsetX_dpad  ,  offsetY_dpad, dpad_size, dpad_size, 5)
-        
-        color= myDict['GAMEPAD1_BUTTON_DPAD_UP'] and 7 or 5
-        pyxel.rect(offsetX_dpad  ,  offsetY_dpad - dpad_size, dpad_size, dpad_size, color)
-
-        color= myDict['GAMEPAD1_BUTTON_DPAD_DOWN'] and 7 or 5
-        pyxel.rect(offsetX_dpad  ,  offsetY_dpad +dpad_size, dpad_size,dpad_size, color)
-
-        color= myDict['GAMEPAD1_BUTTON_DPAD_LEFT'] and 7 or 5
-        pyxel.rect(offsetX_dpad - dpad_size  ,  offsetY_dpad, dpad_size,dpad_size, color)
-
-        color= myDict['GAMEPAD1_BUTTON_DPAD_RIGHT'] and 7 or 5
-        pyxel.rect(offsetX_dpad + dpad_size  ,  offsetY_dpad, dpad_size,dpad_size, color)
-
-        # buttons
-        buttonsize=2
-        offsetX_button=offsetX+gapH
-        offsetY_button=offsetY_dpad
-        color= myDict['GAMEPAD1_BUTTON_A'] and 7 or 5
-        pyxel.circ(offsetX_button, offsetY_button + buttonsize+2, buttonsize, color)
-
-        color= myDict['GAMEPAD1_BUTTON_Y'] and 7 or 5
-        pyxel.circ(offsetX_button, offsetY_button - buttonsize-2, buttonsize, color)
-        
-        color= myDict['GAMEPAD1_BUTTON_B'] and 7 or 5
-        pyxel.circ(offsetX_button+ buttonsize+2, offsetY_button, buttonsize, color)
-
-        color= myDict['GAMEPAD1_BUTTON_X'] and 7 or 5
-        pyxel.circ(offsetX_button-buttonsize-2, offsetY_button, buttonsize, color)
+        pyxel.blt(200, 10, image_base, 0, 0, 16, 16, 0)         # gamepad base image
+        pyxel.blt(200, 10, image_triggered, 0 ,0, 16, 16, 0)    # gamepad trigger image
 
         pyxel.text(200,40, f'wip',  1)
 
