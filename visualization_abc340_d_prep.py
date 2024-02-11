6f3ad1b04fc3510021bbb7baa5ba833b9e705daf
jamad <jamad2001@gmail.com>
2024-02-11 22:32:38 +0200

    Update visualization_abc340_d_prep.py

--- visualization_abc340_d_prep.py
+++ visualization_abc340_d_prep.py
@@ -2,53 +2,29 @@ import pygame
 from sys import exit
 import numpy as np
 
+pygame.init()
 width = 800
 height = 600
-pygame.init()
 screen = pygame.display.set_mode((width, height), 0, 32)
-pygame.display.set_caption("CompGraph_Midterm - Indra Imanuel Gunawan - 20195118")
+#pygame.display.set_caption("CompGraph_Midterm - Indra Imanuel Gunawan - 20195118")
 
-# Define the colors we will use in RGB format
-BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-margin = 6
+margin = 8
 pts = []
-knots = []
 count = 0
-screen.fill(WHITE)
 
 # https://kite.com/python/docs/pygame.Surface.blit
 clock = pygame.time.Clock()
 
-#Lagrange
-def lagrange():
-    print('lagrange')
-    #Line and rects
-    for i in range(count - 1):        pygame.draw.line(screen, GREEN, pts[i], pts[i+1], 3)
-    for i in range(count):        pygame.draw.rect(screen, BLUE, (pts[i][0] - margin, pts[i][1] - margin, 2 * margin, 2 * margin), 5)
-
-    for t in np.arange(0, len(pts)-1, 0.01):
-        ptPlt = np.zeros(2, float)
-        for i in np.arange(0, len(pts), 1):
-            num, den = 1, 1
-            for j in np.arange(0, len(pts), 1):
-                if j != i:
-                    num = num * (t - j)
-                    den = den * (i - j)
-            ptPlt = ptPlt + np.dot(pts[i], num/den)
-        pygame.draw.circle(screen, RED, ptPlt.astype(int), 3)
-
-def drawPolylines(color='GREEN', thick=3):
-    if (count < 2): return
-    for i in range(count - 1):  pygame.draw.line(screen, color, pts[i], pts[i+1], thick)
-    for i in range(count):      pygame.draw.rect(screen, BLUE, (pts[i][0] - margin, pts[i][1] - margin, 2 * margin, 2 * margin), 5)
-    lagrange()
 
 done = False
+
+
+screen.fill(WHITE)
 while not done:
     
     time_passed = clock.tick(60)
@@ -63,9 +39,26 @@ while not done:
                 
                 pygame.draw.rect(screen, BLUE, (pt[0] - margin, pt[1] - margin, 2 * margin, 2 * margin), 5)      
 
-            if len(pts) > 1:
+            if 2<=count: # points to join
                 screen.fill(WHITE)
-                drawPolylines(GREEN, 3)
+
+                #lagrange
+                for t in np.arange(0, len(pts)-1, 0.01):
+                    ptPlt = np.zeros(2, float)
+                    for i in np.arange(0, len(pts), 1):
+                        num, den = 1, 1
+                        for j in np.arange(0, len(pts), 1):
+                            if j != i:
+                                num = num * (t - j)
+                                den = den * (i - j)
+                        ptPlt = ptPlt + np.dot(pts[i], num/den)
+                    pygame.draw.circle(screen, RED, ptPlt.astype(int), 3)
+
+                #Line and rects
+                for i in range(count - 1):  pygame.draw.line(screen, 'GREEN', pts[i], pts[i+1], 3)
+
+                for x,y in pts:pygame.draw.rect(screen, BLUE, (x - margin, y - margin, 2 * margin, 2 * margin), 5)
+
         elif event.type == pygame.QUIT:             done = True
 
     pygame.display.update()
