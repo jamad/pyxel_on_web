167ed39de27c100889a84d124e54759b08060d4f
jamad <jamad2001@gmail.com>
2024-02-11 22:00:31 +0200

    Update visualization_abc340_d_prep.py

--- visualization_abc340_d_prep.py
+++ visualization_abc340_d_prep.py
@@ -6,8 +6,6 @@ width = 800
 height = 600
 pygame.init()
 screen = pygame.display.set_mode((width, height), 0, 32)
-
-screen = pygame.display.set_mode((width, height), 0, 32)
 pygame.display.set_caption("CompGraph_Midterm - Indra Imanuel Gunawan - 20195118")
 
 # Define the colors we will use in RGB format
@@ -25,7 +23,9 @@ screen.fill(WHITE)
 # https://kite.com/python/docs/pygame.Surface.blit
 clock = pygame.time.Clock()
 
-def clearAndRedraw():
+#Lagrange
+def lagrange():
+    
     screen.fill(WHITE)
     #Line and rects
     for i in range(count - 1):
@@ -33,13 +33,7 @@ def clearAndRedraw():
     for i in range(count):
         pygame.draw.rect(screen, BLUE, (pts[i][0] - margin, pts[i][1] - margin, 2 * margin, 2 * margin), 5)
 
-    #Buttons
-    lagrangeButton.draw(screen, (0, 0, 0))
 
-
-#Lagrange
-def lagrange():
-    clearAndRedraw()
     for t in np.arange(0, len(pts)-1, 0.01):
         ptPlt = np.zeros(2, float)
         for i in np.arange(0, len(pts), 1):
@@ -60,35 +54,6 @@ def drawPolylines(color='GREEN', thick=3):
         if(count > 2):
             lagrange()
 
-#Button Class
-class button():
-    def __init__(self, color, x, y, width, height, text=''):
-        self.color = color
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.text = text
-
-    def draw(self, win, outline=None):
-        if outline:
-            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
-
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
-
-        if self.text != '':
-            font = pygame.font.SysFont('comicsans', 30)
-            text = font.render(self.text, 1, (0, 0, 0))
-            win.blit(text, (
-            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
-
-    def isOver(self, pos):
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
-                return True
-        return False
-
-# Loop until the user clicks the close button.
 done = False
 pressed = 0
 margin = 6
@@ -97,10 +62,8 @@ old_button1 = 0
 old_button3 = 0
 
 selectedPoint = -1
-lagrangeButton = button((255,165,0),650,100,120,70,"Lagrange")
 
 while not done:
-    lagrangeButton.draw(screen, (0, 0, 0))
 
     time_passed = clock.tick(30)
 
@@ -120,7 +83,6 @@ while not done:
 
     if old_pressed == -1 and pressed == 1 and old_button1 == 1 and button1 == 0:
         print('mouse click')
-        #Mouse click
         if x < 590:
             pts.append(pt)
             count += 1
@@ -129,8 +91,6 @@ while not done:
     if len(pts) > 1:
         drawPolylines(GREEN, 3)
 
-    # Go ahead and update the screen with what we've drawn.
-    # This MUST happen after all the other drawing commands.
     pygame.display.update()
     old_button1 = button1
     old_pressed = pressed
